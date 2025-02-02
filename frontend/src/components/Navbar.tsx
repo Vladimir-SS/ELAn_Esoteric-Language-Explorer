9b0b99006eb6f36df88a896ab2345f90814656e4
Vladimir-SS <stefansbarcea@yahoo.com>
2025-02-02 18:08:02 +0200

    Changed link for documentation in navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,44 +1,49 @@
 import { Link } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
-    return (
-      <nav
-        className="navbar navbar-expand-lg navbar-light"
-        style={{ backgroundColor: "#282c30" }}
-      >
-        <div className="container-fluid">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link to="/" className="nav-link">
-                Esolangs
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/esolangs/category" className="nav-link">
-                Categories
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/esolangs/paradigm" className="nav-link">
-                Paradigms
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/compare" className="nav-link">
-                Compare
-              </Link>
-            </li>
-          </ul>
-          <ul>
-            <li className="nav-item">
-              <Link to="/documentation" className="nav-link">
-                Documentation
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
+  return (
+    <nav
+      className="navbar navbar-expand-lg navbar-light"
+      style={{ backgroundColor: "#282c30" }}
+    >
+      <div className="container-fluid">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Esolangs
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/esolangs/category" className="nav-link">
+              Categories
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/esolangs/paradigm" className="nav-link">
+              Paradigms
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/compare" className="nav-link">
+              Compare
+            </Link>
+          </li>
+        </ul>
+        <ul>
+          <li className="nav-item">
+            <a
+              href="https://vladimir-ss.github.io/ELAn_Esoteric-Language-Explorer/"
+              className="nav-link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Documentation
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
